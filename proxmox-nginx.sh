6bae332b54d6faf0fc1742fcfd2502a1afc803a6
Andy Ismail <98675756+simunda@users.noreply.github.com>
2022-10-11 06:07:03 +0700

    Create proxmox-nginx.sh

---
+++ proxmox-nginx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "install nginx"
+sleep 2s
+apt install nginx -y >> ~/proxmox-internet.log 2>&1
+
+echo "rm /etc/nginx/sites-enabled/default"
+sleep 2s
+rm /etc/nginx/sites-enabled/default >> ~/proxmox-internet.log 2>&1
+
+echo "wget https://raw.githubusercontent.com/simunda/catatan/main/proxmox.conf"
+sleep 2s
+wget https://raw.githubusercontent.com/simunda/catatan/main/proxmox.conf -P /etc/nginx/conf.d/ >> ~/proxmox-internet.log 2>&1
+
+echo "nginx -t"
+sleep 2s
+nginx -t >> ~/proxmox-internet.log 2>&1
+
+echo "systemctl restart nginx"
+sleep 2s
+systemctl restart nginx
+
+echo "DONE!"
+sleep 2s
